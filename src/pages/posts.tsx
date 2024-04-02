3f1224bc7c0d64b702e4033a0182fe21172c7ffe
Nikola Pervic Bogdanovic <pervicnikola@gmail.com>
2024-04-02 20:28:22 +0200

    Fetch and render posts,comments and users on the <Posts />

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,3 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
+import type { Comment, Post, User } from "../api/types";
+import { QueryKeys } from "../constants";
+
 const COMPONENT_NAME = "<Posts />";
 
 interface Props {
@@ -7,7 +11,31 @@ interface Props {
 function Posts({ helloMessage }: Props) {
 	console.log(`${helloMessage} ${COMPONENT_NAME}.`);
 
-	return <div>Hello world from Posts!</div>;
+	const { data: posts } = useQuery<Post[]>({ queryKey: [QueryKeys.POSTS] });
+	const { data: comments } = useQuery<Comment[]>({
+		queryKey: [QueryKeys.COMMENTS],
+	});
+	const { data: users } = useQuery<User[]>({ queryKey: [QueryKeys.USERS] });
+
+	return posts?.map(({ id: postId, title, body, userId }) => (
+		<article key={postId} className="p-4 flex justify-between">
+			<div>
+				<h2 className="text-xl capitalize">{title}</h2>
+				<p className="text-base capitalize">{body}</p>
+			</div>
+			<div>
+				<div>Author: {users?.find(({ id }) => id === userId)?.name}</div>
+				<div>
+					Number of comments:{" "}
+					{
+						comments?.filter(
+							({ postId: commentPostId }) => commentPostId === postId,
+						).length
+					}
+				</div>
+			</div>
+		</article>
+	));
 }
 
 export default Posts;
